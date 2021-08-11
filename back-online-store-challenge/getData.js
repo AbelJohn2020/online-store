514573b9e6ba6af1aa4559ba03471bbb3b593292
Abel AR <abel_john_14@hotmail.com>
2021-08-11 14:01:18 -0500

    Get data products in back

--- back-online-store-challenge/getData.js
+++ back-online-store-challenge/getData.js
@@ -1,2 +1,16 @@
 const base_uri = "https://kqxty15mpg.execute-api.us-east-1.amazonaws.com/";
 const url_products = `${base_uri}products`;
+const csvtojson = require('csvtojson');
+const request = require('request');
+
+module.exports = {
+    getDataProducts: function () {
+        const csvStr = request.get(url_products);
+
+        return csvtojson({delimiter: "'", noheader:true, output:'json'})
+            .fromStream(csvStr)
+            .subscribe((csv)=> csv)
+            .then(data => data)
+            .catch(e => e)
+    }
+}
--- front-online-store-challenge/src/components/utils/utils.js
+++ front-online-store-challenge/src/components/utils/utils.js
@@ -2,7 +2,7 @@ import { base_uri } from "../../app/config"
 
 
 export const getProductsData = async () => {
-    const url = await fetch(`${base_uri}products`);
+    const url = await fetch(`${base_uri}buyers`);
     const products = await url.json();
     console.console.log((products));
 }
