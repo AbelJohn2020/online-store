89408d007f1363563772df635fea8248ccb85c27
Abel AR <abel_john_14@hotmail.com>
2021-08-13 21:12:30 -0500

    Read data and create state with the names of the informations

--- front-online-store-challenge/src/App.js
+++ front-online-store-challenge/src/App.js
@@ -1,8 +1,48 @@
-import React from "react";
-import { getProductsData } from "./components/utils/utils";
+import React, { useEffect, useState } from "react";
+import { getBuyersData, getProductsData, getTransactionsData } from "./components/utils/utils";
 
 function App() {
-  console.log(getProductsData());
+  const [products, setProducts] = useState({
+    data:[],
+    loading: true,
+  });
+
+  const [buyers, setBuyers] = useState({
+    data:[],
+    loading: true,
+  });
+
+  const [transactions, setTransactions] = useState({
+    data:[],
+    loading: true,
+  });
+
+  useEffect(() => {
+    getProductsData()
+      .then(res => setProducts({
+        data: res,
+        loading: false,
+      }))
+      .catch(e => e);
+
+    getBuyersData()
+      .then(res => setBuyers({
+        data: res,
+        loading: false,
+      }))
+      .catch(e => e);
+
+    getTransactionsData()
+      .then(res => setTransactions({
+        data: res,
+        loading: false,
+      }))
+      .catch(e => e)
+  }, []);
+
+  console.log(products);
+  console.log(buyers);
+  console.log(transactions);
   return (
     <div className="App">
       <h1>Online store Challenge</h1>
--- front-online-store-challenge/src/components/utils/utils.js
+++ front-online-store-challenge/src/components/utils/utils.js
@@ -1,8 +1,21 @@
-import { base_uri } from "../../app/config"
+import { base_uri } from "../../app/config";
+import axios from 'axios';
 
 
 export const getProductsData = async () => {
-    const url = await fetch(`${base_uri}transactions`);
-    const products = await url.json();
-    console.console.log((products));
+    const url = await axios(`${base_uri}products`);
+    const products = await url.data;
+    return products;
+}
+
+export const getBuyersData = async () => {
+    const url = await axios(`${base_uri}buyers`);
+    const buyers = await url.data;
+    return buyers;
+}
+
+export const getTransactionsData = async () => {
+    const url = await axios(`${base_uri}transactions`);
+    const transactions = await url.data;
+    return transactions;
 }
