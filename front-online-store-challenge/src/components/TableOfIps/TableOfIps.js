21a83e24ab4686fbf955c3f8dd979825affb0652
Abel AR <abel_john_14@hotmail.com>
2021-08-15 15:18:58 -0500

    Add recomended products in the Ips table

--- front-online-store-challenge/src/components/TableOfIps/TableOfIps.js
+++ front-online-store-challenge/src/components/TableOfIps/TableOfIps.js
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react'
-import { BuyerRequest, transactionsTable } from '../utils/filterData';
+import { BuyerRequest, ProductsTable, transactionsTable } from '../utils/filterData';
 import nextId from "react-id-generator";
 import Loading from '../Loaging/Loading';
 import { Container, Table, TdBody, ThHeader, TrBody, TrHeader } from '../TableBuyers/TableBuyersStyles';
+import { Li, Ul } from '../IdsProducts/ListStyles';
+import { ThHeaderCenter } from './TableOfIpsStyles';
 
 const TableOfIps = ({ip, shadow}) => {
     const [transactions, setTransactions] = useState({
@@ -11,19 +13,27 @@ const TableOfIps = ({ip, shadow}) => {
     });
     const [buyers, setBuyers] = useState({
         dataB:[],
-      });
+        loadingB: true,
+    });
+
+    const [products, setProducts] = useState({
+        dataP:[],
+        loadingP: true,
+    })
 
     const { dataB } = buyers;
+    const { dataP } = products
     const { dataT, loadingT} = transactions;
   
     useEffect(() => {
         transactionsTable(setTransactions)
         BuyerRequest(setBuyers);
+        ProductsTable(setProducts);
     }, []);
 
     const getPartnertsWithTheSameIp = () => {
         return dataT.filter( transaction => transaction.ip === ip)
-    }
+    };
 
     const getPartnerts = getPartnertsWithTheSameIp()
     
@@ -34,9 +44,32 @@ const TableOfIps = ({ip, shadow}) => {
         } else {
             return getPartnerts;
         }
-    }
+    };
     
     const getNames = compare();
+
+    const getidProductsByTransactions = () => {
+        if(getPartnerts.length !== 0) {
+            const filterTransactions = getPartnerts.map( transaction => transaction["product ids"])
+            return dataP.filter( product => filterTransactions.includes(product['id']))
+        } else {
+            return getPartnerts;
+        }
+    };
+
+    const recomendProducts = getidProductsByTransactions();
+
+    const getArrBuyersPLusProducts = () => {
+        return getNames.map(partnert => {
+            return {
+             partnert: partnert.name,
+             product: recomendProducts.map( ({name}) => name),
+             price: recomendProducts.map( ({price}) => price)
+            }
+        })
+    };
+
+    const recomendations = getArrBuyersPLusProducts();
 
     return (
         <Container>
@@ -51,13 +84,33 @@ const TableOfIps = ({ip, shadow}) => {
                                 <thead>
                                     <TrHeader>
                                         <ThHeader>names</ThHeader>
+                                        <ThHeaderCenter>product</ThHeaderCenter>
+                                        <ThHeaderCenter>price</ThHeaderCenter>
                                     </TrHeader>
                                 </thead>
                                 <tbody>
                                     {
-                                        getNames.map( partnert => (
+                                        recomendations.map(({partnert, product, price}) => (
                                             <TrBody key={nextId()} shadow={shadow}>
-                                                <TdBody><b>{partnert.name}</b></TdBody>
+                                                <TdBody>{partnert}</TdBody>
+                                                <TdBody>
+                                                    <Ul>
+                                                        {
+                                                            product.map( prod => (
+                                                                <Li key={nextId()}>{prod}</Li>
+                                                            ))
+                                                        }
+                                                    </Ul>
+                                                </TdBody>
+                                                <TdBody>
+                                                    <Ul>
+                                                        {
+                                                            price.map( pric => (
+                                                                <Li key={nextId()}>{pric}</Li>
+                                                            ))
+                                                        }
+                                                    </Ul>
+                                                </TdBody>
                                             </TrBody>
                                         ))
                                     }
---
+++ front-online-store-challenge/src/components/TableOfIps/TableOfIpsStyles.js
@@ -0,0 +1,9 @@
+import styled from "@emotion/styled";
+
+
+export const ThHeaderCenter = styled.th`
+    box-sizing: border-box;
+    padding: 12px 16px;
+    text-align: center;
+    text-transform: capitalize;
+`;
