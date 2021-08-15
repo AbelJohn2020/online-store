3707fbad8272fd5bae1134f01e10e18256470ebb
Abel AR <abel_john_14@hotmail.com>
2021-08-14 19:51:41 -0500

    Add pagination in groups of 20 buyers

--- front-online-store-challenge/src/components/TableBuyers/TableBuyers.js
+++ front-online-store-challenge/src/components/TableBuyers/TableBuyers.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom';
 import { filterDataB } from '../utils/filterData';
 import { getBuyersData } from '../utils/utils';
 
@@ -7,6 +8,7 @@ const TableBuyers = () => {
         dataB:[],
         loadingB: true,
     });
+    const [pagination, setPagination] = useState(0)
     const { dataB, loadingB } = buyers;
     
     useEffect(() => {
@@ -18,7 +20,29 @@ const TableBuyers = () => {
         .catch(e => e);
     }, []);
 
-    const dataBuyers = filterDataB(dataB);
+    
+    const filterDataBuyers = () => {
+        const data = filterDataB(dataB);
+        return data.slice(pagination, pagination+20)
+    }
+
+    const handleNextBlock = () => {
+        if(pagination < 700) {
+            setPagination(pagination+20)
+        } else {
+            setPagination(0)
+        }
+    }
+
+    const handlePreviousBlock = () => {
+        if(pagination > 0) {
+            setPagination(pagination-20)
+        } else {
+            setPagination(700)
+        }
+    }
+
+    const dataBuyers = filterDataBuyers()
     return (
         <div>
             {
@@ -28,6 +52,11 @@ const TableBuyers = () => {
                     </div>
                 :   <div>
                         <h1>Table of Clients that made purchases on the plataform</h1>
+                        <hr />
+                        <div>
+                            <button onClick={() => handlePreviousBlock()}>prev</button>
+                            <button onClick={() => handleNextBlock()}>next</button>
+                        </div>
                         <div>
                             <table>
                                 <thead>
@@ -41,7 +70,9 @@ const TableBuyers = () => {
                                     dataBuyers.map( ({id, name, age}) => (
                                         <tr key={id}>
                                             <td>
-                                                {name}
+                                                <Link to={`/buyer/${id}`} className="name">
+                                                    {name}
+                                                </Link>
                                             </td>
                                             <td>{age}</td>
                                         </tr>
